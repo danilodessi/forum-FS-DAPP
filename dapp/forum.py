b15d47d70a4b0fc5fb5470673e8473a46dbf86ca
danilodessi <dandes92@live.it>
2016-03-04 12:37:44 +0100

    Update forum.py

--- dapp/forum.py
+++ dapp/forum.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015 Davide Gessa
+# Copyright (c) 2016 Danilo Dessì
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
